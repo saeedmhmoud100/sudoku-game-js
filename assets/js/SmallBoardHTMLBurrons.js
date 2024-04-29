124a6c1cae1a60f0141fc428e98d2b137221b38c
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-04-29 22:39:46 +0300

    create check row before drop function

--- assets/js/SmallBoardHTMLBurrons.js
+++ assets/js/SmallBoardHTMLBurrons.js
@@ -13,6 +13,8 @@ class SmallBoardHTMLButtons {
         boardItem.setAttribute('data-board', this.board_id.toString());
         boardItem.setAttribute('data-button', button_id.toString());
         boardItem.id = "button-" + this.board_id.toString() + "-" + button_id.toString();
+        boardItem.setAttribute("data-button-count", SmallBoardHTMLButtons.button_count.toString());
+        SmallBoardHTMLButtons.button_count++;
         boardItem.addEventListener('dragover', this.dragOver);
         boardItem.addEventListener('dragleave', this.dragLeave);
         boardItem.addEventListener('drop', this.drop);
@@ -21,6 +23,40 @@ class SmallBoardHTMLButtons {
     drop(event) {
         event.preventDefault();
         const ele = document.getElementById(event.dataTransfer.getData("text/plain"));
+        function checkPlaceRows(val, target) {
+            let start_num = target.getAttribute('data-button-count');
+            let board_num = Math.floor(start_num / 9);
+            let row_num = (start_num % 9);
+            if (row_num == 1 || row_num == 2 || row_num == 0) {
+                row_num = 0;
+            }
+            else if (row_num == 4 || row_num == 5 || row_num == 3) {
+                row_num = 1;
+            }
+            else if (row_num == 7 || row_num == 8 || row_num == 6) {
+                row_num = 2;
+            }
+            if (board_num == 0 || board_num == 1 || board_num == 2)
+                start_num = 0;
+            else if (board_num == 3 || board_num == 4 || board_num == 5)
+                start_num = 27;
+            if (board_num == 6 || board_num == 7 || board_num == 8)
+                start_num = 54;
+            start_num += row_num * 3;
+            for (let i = +start_num; i < +start_num + 27; i += 9) {
+                for (let j = i; j < i + 3; j++) {
+                    const search = document.querySelector(`div[data-button-count="${j}"]`);
+                    if (search.innerText === val) {
+                        console.log("found");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        if (checkPlaceRows(ele.innerText, event.target)) {
+            return;
+        }
         if (ele.getAttribute('data-board') === event.target.getAttribute('data-board')) {
             if (event.target.innerText === "") {
                 event.target.innerText = ele.innerText;
@@ -45,4 +81,5 @@ class SmallBoardHTMLButtons {
         return this.buttons;
     }
 }
+SmallBoardHTMLButtons.button_count = 0;
 export default SmallBoardHTMLButtons;
--- assets/ts/SmallBoardHTMLBurrons.ts
+++ assets/ts/SmallBoardHTMLBurrons.ts
@@ -1,6 +1,7 @@
 class SmallBoardHTMLButtons{
     buttons: HTMLDivElement[];
     public board_id: number;
+    public static button_count: number;
     constructor(board_id:number){
         this.board_id = board_id;
 
@@ -19,16 +20,63 @@ class SmallBoardHTMLButtons{
         boardItem.setAttribute('data-board', this.board_id.toString());
         boardItem.setAttribute('data-button', button_id.toString());
         boardItem.id = "button-" + this.board_id.toString() + "-" + button_id.toString()
-
+        boardItem.setAttribute("data-button-count", SmallBoardHTMLButtons.button_count.toString());
+        SmallBoardHTMLButtons.button_count++;
         boardItem.addEventListener('dragover', this.dragOver);
         boardItem.addEventListener('dragleave', this.dragLeave);
         boardItem.addEventListener('drop', this.drop);
         return boardItem;
     }
 
+
+
+
     drop(event: DragEvent): void {
         event.preventDefault();
         const ele = document!.getElementById(event.dataTransfer!.getData("text/plain"));
+        function checkPlaceRows(val:string, target:HTMLElement) {
+
+            let start_num: any= target.getAttribute('data-button-count');
+
+
+            let board_num: any= Math.floor(start_num / 9) ;
+            let row_num: any= (start_num % 9) ;
+
+            if(row_num == 1 ||row_num==2||row_num==0 ) {
+                row_num=0;
+            }else if(row_num == 4 ||row_num==5||row_num==3 ) {
+                row_num=1;
+            }else if(row_num == 7 ||row_num==8||row_num== 6) {
+                row_num=2;
+            }
+
+            if(board_num == 0 || board_num == 1 || board_num == 2)
+                start_num = 0;
+            else if(board_num == 3 || board_num == 4 || board_num == 5)
+                start_num = 27;
+            if(board_num == 6 || board_num == 7 || board_num == 8)
+                start_num = 54;
+
+            start_num += row_num*3;
+            for (let i = +start_num; i < +start_num+27; i +=9) {
+                for (let j = i; j < i+3; j++) {
+                    const search: HTMLElement = <HTMLElement>document.querySelector(`div[data-button-count="${j}"]`);
+                    if(search!.innerText === val){
+                        console.log("found")
+                        return true;
+
+                    }
+                }
+            }
+
+            return false
+        }
+
+
+        if(checkPlaceRows(ele!.innerText, (<HTMLElement>event.target))){
+            return;
+        }
+
 
         if(ele!.getAttribute('data-board') === (<HTMLElement>event.target).getAttribute('data-board')){
             if((<HTMLElement>event.target).innerText === "") {
@@ -47,6 +95,8 @@ class SmallBoardHTMLButtons{
     }
 
 
+
+
     dragOver(event: DragEvent): void {
         event.preventDefault();
         (<HTMLElement>event.target)!.classList.add("drag-over");
@@ -55,14 +105,12 @@ class SmallBoardHTMLButtons{
     dragLeave(event: DragEvent): void {
         (<HTMLElement>event.target)!.classList.remove("drag-over");
     }
-
-
-
-
     public getButtons(): HTMLDivElement[] {
         return this.buttons;
     }
 
+
 }
 
+SmallBoardHTMLButtons.button_count = 0;
 export default SmallBoardHTMLButtons;
